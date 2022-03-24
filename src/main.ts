67b58dd181e441cb115b48dad26d6aea18eca18c
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-03-24 15:16:48 +0700

    Debug running file

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const myInput = core.getInput('myInput');
+    core.debug(`Hello ${myInput} from inside a container`);
+
+    // Get github context data
+    const context = github.context;
+    console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+  } catch (error) {
+    let message = 'Unknown Error'
+    if (error instanceof Error) message = error.message
+    core.setFailed(message);
+  }
+}
+
+run();
